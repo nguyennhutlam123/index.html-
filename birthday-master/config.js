d87053c046e40d7e521b2067190379bc9331b4ce
nguyennhutlam123 <nguyennhutlam9a3@gmail.com>
2023-12-02 09:36:17 +0700

    index.html

---
+++ birthday-master/config.js
@@ -0,0 +1,29 @@
+
+var config = {
+   
+    texts: [
+        " hãy cùng ước cùng mình nào ",      
+        "1",  
+        "2",
+        "3",
+        " happy birthday to me ",
+        " Chúc mừng sinh nhật của tôi_Nhứt Lâm ",
+
+
+    ],
+    
+    imgs: {
+        "心爱的小可爱": "imgs/z4567295521387_f369140b597b7259ea3a6d52223104e1.jpg",
+    },
+    desc: {
+
+        turn_on: " Cùng bắt đầu thôi nào  ",
+        play: "bật nhạc lên nha ",
+        bannar_coming: "tiếp theo nữa nè  ",
+        balloons_flying: "có vẻ như thiếu một cái gì đó ",
+        cake_fadein: " bánh sinh nhật  ",
+        light_candle: "và còn nến nữa ",
+        wish_message: "happy birthdat to me ",
+        story: "Hãy bắt đầu tuổi 18 cùng Nhứt Lâm nào ! ",
+    }
+};
